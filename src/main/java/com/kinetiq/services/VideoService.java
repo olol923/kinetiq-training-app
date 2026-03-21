a909d1f58ff75302e4a2bf9f870db7a0caf83a29
olol923 <shumazu061@gmail.com>
2026-03-21 19:55:55 +0800

    Add VideoService class with video business logic

---
+++ src/main/java/com/kinetiq/services/VideoService.java
@@ -0,0 +1,24 @@
+package com.kinetiq.services;
+
+public class VideoService {
+
+    public void playVideo(String videoId) {
+        // Logic to play a video
+        System.out.println("Playing video: " + videoId);
+    }
+
+    public void pauseVideo(String videoId) {
+        // Logic to pause a video
+        System.out.println("Pausing video: " + videoId);
+    }
+
+    public void stopVideo(String videoId) {
+        // Logic to stop a video
+        System.out.println("Stopping video: " + videoId);
+    }
+
+    public void uploadVideo(String videoId, String videoData) {
+        // Logic to upload a video
+        System.out.println("Uploading video: " + videoId);
+    }
+}
