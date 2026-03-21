7760115747a00dc52b601688ac8dd1624b01f23d
olol923 <shumazu061@gmail.com>
2026-03-21 19:56:43 +0800

    Add Constants.java file with application constants

---
+++ src/main/java/com/kinetiq/utils/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+    public static final String APP_NAME = "Kinetiq Training App";
+    public static final String VERSION = "1.0.0";
+    public static final String DATE_CREATED = "2026-03-21 11:53:25";
+    
+    // Add more constants here as needed
+}
